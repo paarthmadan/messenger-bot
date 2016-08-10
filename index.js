0989e26209534adfaa2d2d7e49918fc9bb079a7e
paarthmadan <madanhockey@hotmail.com>
2016-08-10 15:24:16 -0400

    created function to parse return method for keywords
    added /paarth command to return special message

--- index.js
+++ index.js
@@ -6,11 +6,31 @@ login({email: config.fb.email, password: config.fb.password}, function callback(
 
 	if(err) return console.error(err);
 
+	api.setOptions({
+		logLevel:"silent"
+	});
+
 	api.listen(function callback(err, msg){
-		
-		api.sendMessage("Bot says: " + msg.body, msg.threadID);
+
+		var currentMessage = msg.body;
+
+		if(includes("/paarth", currentMessage)){
+			api.sendMessage({body:"Paarth is my creator!"}, msg.threadID, function callback(err, messageInfo){
+				if(err) return console.error(err);
+			});
+		}else{
+			api.sendMessage("Bot Says: " + msg.body, msg.threadID);
+		}
 
 	});
 
 
 });
+
+function includes(string, msg){
+	if(msg.toLowerCase().indexOf(string.toLowerCase()) != -1){
+		return true;
+	}else{
+		return false;
+	}
+}
