d5b3f2acf478b7a09364c0bfb784ef5758bb6410
paarthmadan <madanhockey@hotmail.com>
2016-08-10 14:56:35 -0400

    formatted output

--- index.js
+++ index.js
@@ -5,8 +5,11 @@ var config = require('./config');
 login({email: config.fb.email, password: config.fb.password}, function callback(err, api){
 
 	if(err) return console.error(err);
+
 	api.listen(function callback(err, msg){
-		api.sendMessage(message.body, message.threadId);
+		
+		api.sendMessage("Bot says: " + msg.body, msg.threadID);
+
 	});
 
 
