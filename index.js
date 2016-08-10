52e62fa88455c0727457e82ba935847bfa1f3279
paarthmadan <madanhockey@hotmail.com>
2016-08-10 14:46:05 -0400

    created login function, and basic echo bot
    added credentials into hidden config.js file, and imported it in index.js

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-//initial commit
+var login = require('facebook-chat-api');
+var config = require('./config');
+
+
+login({email: config.fb.email, password: config.fb.password}, function callback(err, api){
+
+	if(err) return console.error(err);
+	api.listen(function callback(err, msg){
+		api.sendMessage(message.body, message.threadId);
+	});
+
+
+});
