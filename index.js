b2d622d28c294a7834aa8f3a4fc09be28fa49feb
paarthmadan <madanhockey@hotmail.com>
2016-08-10 23:28:34 -0400

    added special colour keyword, that allows hex input

--- index.js
+++ index.js
@@ -12,12 +12,33 @@ login({email: config.fb.email, password: config.fb.password}, function callback(
 
 	api.listen(function callback(err, msg){
 
+		if(err) return console.error(err);
+
 		var currentMessage = msg.body;
+
+		console.log(currentMessage);
 
 		if(includes("/paarth", currentMessage)){
 			api.sendMessage({body:"Paarth is my creator!"}, msg.threadID, function callback(err, messageInfo){
 				if(err) return console.error(err);
 			});
+		}else if(includes("/colour", currentMessage)){
+			var startOfColour = currentMessage.indexOf("#");
+			
+			var hexCode = "#000000";
+			
+			try{
+			hexCode = currentMessage.substring(startOfColour, startOfColour + 7);
+			}catch(err){
+				console.error(err);
+			}	
+			console.log(hexCode);
+
+			api.sendMessage({body:"Changing chat colour to " + hexCode}, msg.threadID);
+			api.changeThreadColor(hexCode, msg.threadID, function callback(err){
+				if(err) return console.error(err);
+			});
+
 		}else{
 			api.sendMessage("Bot Says: " + msg.body, msg.threadID);
 		}
